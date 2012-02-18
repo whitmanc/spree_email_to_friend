999ab99f42b95ab4de46a28178ee356c5013b343
Dhawal <dhawaljshah@gmail.com>
2012-02-18 21:41:23 +0800

    Update app/controllers/spree/email_sender_controller.rb

--- app/controllers/spree/email_sender_controller.rb
+++ app/controllers/spree/email_sender_controller.rb
@@ -49,4 +49,11 @@ class Spree::EmailSenderController < Spree::BaseController
         @object ||= class_name.get_by_param(params[:id])
       end
     end
+    
+    def after_delivering_affiliate_mail
+      emails = @mail_to_friend.recipient_email.split(/,\s?/)
+      emails.each do |email|
+        current_user.affiliates.create(:affiliate_email => email) if Affiliate.where(:affiliate_email => email).count == 0
+      end
+    end
 end
